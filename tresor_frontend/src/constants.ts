871e1c55e2fbc6c1aab45d3788029e90ca826883
khalil <mohamedkhalilkt@gmail.com>
2024-06-30 03:12:16 +0100

    adas

--- tresor_frontend/src/constants.ts
+++ tresor_frontend/src/constants.ts
@@ -1,2 +1,2 @@
-export const rootUrl = "http://localhost:8000/";
-// export const rootUrl = "http://38.180.157.173:8000/";
+// export const rootUrl = "http://localhost:8000/";
+export const rootUrl = "http://38.180.157.173:8000/";
